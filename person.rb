2630493451c7b95d3797a84618ef3d98cdab2e7a
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-31 16:32:57 +0100

    Decorate a class

--- person.rb
+++ person.rb
@@ -1,5 +1,40 @@
+# code start here
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'You must implement this method'
+  end
+end
+
+class BaseDecorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  attr_accessor :nameable
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    super.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+    super.each_char.first(10).join
+    else
+    super
+    end
+  end
+end
+
 # Create Person class with constructor using parameters => name, age, and parent_permission
-class Person
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = rand(1..100)
     @name = name
@@ -25,4 +60,15 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+  person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+  capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  puts capitalizedTrimmedPerson.correct_name
