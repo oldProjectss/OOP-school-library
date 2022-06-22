05427a665621bcc8971bc55c478a451d3671ee17
Ismail Courr <contact@ismailcourr.ml>
2022-06-23 00:43:41 +0100

    Store datat for people

--- file_save.rb
+++ file_save.rb
@@ -1,10 +1,22 @@
 module SaveToFile
   def write_to_file
     books = {}
+    people = {}
     rentals={}
     @books.each { |book| books[book.title] = book.author }
-    @rentals.each { |rental| rentals[rental.book.title] = {'date' => rental.date, 'person' => rental.person.name }}
+    @people.each do |person|
+      people[person] = if person.instance_of?(Teacher)
+                         { 'name' => person.name, 'specialization' => person.specialization, 'age' => person.age,
+                           'parent_permission' => person.parent_permission, 'id' => person.id }
+                       else
+                         { 'name' => person.name, 'age' => person.age,
+                           'parent_permission' => person.parent_permission, 'id' => person.id }
+
+                       end
+    end
+    # @rentals.each { |rental| rentals[rental.book.title] = {'date' => rental.date, 'person' => rental.person.name }}
     File.write('./data/books.json', JSON.generate(books), mode: 'a')
+    File.write('./data/people.json', JSON.generate(people), mode: 'a')
     File.write('./data/rentals.json', JSON.generate(rentals), mode: 'a')
     exit
   end
@@ -15,10 +27,21 @@ module SaveToFile
       books = JSON.parse(books)
       books.each { |title, author| @books.push(Book.new(title, author)) }
     end
-    if File.exist?('./data/rentals.json')
-      rentals = File.read('./data/rentals.json')
-      rentals = JSON.parse(rentals)
-      rentals.each {|book, person| @rentals.push(Rental.new(book, person["person"],person["date"]))}
+    if File.exist?('./data/people.json')
+    people = File.read('./data/people.json')
+    people = JSON.parse(people)
+    people.each do |_person, prop|
+      if prop['specialization']
+        @people.push(Teacher.new(prop['specialization'], prop['name'], prop['age']))
+      else
+        @people.push(Student.new(prop['age'], prop['name'], prop['parent_permission']))
+      end
     end
+    end
+    # if File.exist?('./data/rentals.json')
+    #   rentals = File.read('./data/rentals.json')
+    #   rentals = JSON.parse(rentals)
+    #   rentals.each {|book, person| @rentals.push(Rental.new(book, person["person"],person["date"]))}
+    # end
   end
 end
