6bee71f0abf7b74145c9dd048a277c064b6b36b5
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-06-23 17:20:15 +0300

    Test for classroom

---
+++ spec/test_classroom_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../classroom'
+describe Classroom do
+    it 'Creates a new classroom correctly' do
+        label = 'our classroom'
+        classroom = Classroom.new(label)
+        expect(classroom.students).to be_empty
+        expect(classroom.label).to eql(label)
+    end
+end
