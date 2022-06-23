c3a0197ebfc797f775c16602a48fd267b85fc3b6
Ismail Courr <contact@ismailcourr.ml>
2022-06-23 12:24:03 +0100

    fix linters

--- file_save.rb
+++ file_save.rb
@@ -14,7 +14,6 @@ module SaveToFile
 
                              end
     end
-    # @rentals.each { |rental| rentals[rental.book.title] = {'date' => rental.date, 'person' => rental.person.name }}
     File.write('./data/books.json', JSON.generate(books), mode: 'w')
     File.write('./data/people.json', JSON.generate(people), mode: 'w')
     File.write('./data/rentals.json', JSON.generate(rentals), mode: 'w')
@@ -27,21 +26,16 @@ module SaveToFile
       books = JSON.parse(books)
       books.each { |_book, prop| @books.push(Book.new(prop['title'], prop['author'])) }
     end
-    if File.exist?('./data/people.json')
-      people = File.read('./data/people.json')
-      people = JSON.parse(people)
-      people.each do |_person, prop|
-        if prop['specialization']
-          @people.push(Teacher.new(prop['specialization'], prop['name'], prop['age']))
-        else
-          @people.push(Student.new(prop['age'], prop['name'], prop['parent_permission']))
-        end
+    return unless File.exist?('./data/people.json')
+
+    people = File.read('./data/people.json')
+    people = JSON.parse(people)
+    people.each do |_person, prop|
+      if prop['specialization']
+        @people.push(Teacher.new(prop['specialization'], prop['name'], prop['age']))
+      else
+        @people.push(Student.new(prop['age'], prop['name'], prop['parent_permission']))
       end
     end
-    # if File.exist?('./data/rentals.json')
-    #   rentals = File.read('./data/rentals.json')
-    #   rentals = JSON.parse(rentals)
-    #   rentals.each {|book, person| @rentals.push(Rental.new(book, person["person"],person["date"]))}
-    # end
   end
 end
